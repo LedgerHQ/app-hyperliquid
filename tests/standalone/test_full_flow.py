65bef2d668cd92a8a3c6ffd4f65c0c9618542b99
Alexandre Paillier <alexandre.paillier@ledger.fr>
2026-03-05 11:37:45 +0100

    Added more Ragger tests

--- tests/standalone/test_full_flow.py
+++ tests/standalone/test_full_flow.py
@@ -1,5 +1,6 @@
 from application_client.action_metadata import ActionMetadata, Network, OperationType
 from application_client.approve_builder_fee import ApproveBuilderFee
+from application_client.bulk_cancel import BulkCancel, CancelRequest
 from application_client.bulk_order import BuilderInfo, BulkOrder, Grouping
 from application_client.command_sender import CommandSender
 from application_client.order_request import Limit, OrderRequest, OrderType, Tif, Trigger, TriggerType
@@ -8,7 +9,7 @@ from application_client.update_leverage import UpdateLeverage
 from ragger.backend.interface import BackendInterface
 
 
-def test_sign_action(backend: BackendInterface) -> None:
+def test_sign_action_limit(backend: BackendInterface) -> None:
     client = CommandSender(backend)
     client.provide_action_metadata(ActionMetadata(
         1,
@@ -74,4 +75,140 @@ def test_sign_action(backend: BackendInterface) -> None:
     ))
     client.sign_action("m/44'/60'/0'/0/0")
     client.sign_action("m/44'/60'/0'/0/1")
+    client.sign_action("m/44'/60'/0'/0/1")
+
+def test_sign_action_trigger(backend: BackendInterface) -> None:
+    client = CommandSender(backend)
+    client.provide_action_metadata(ActionMetadata(
+        1,
+        ActionType.ORDER,
+        42,
+        "BTC",
+        Network.MAINNET))
+    client.set_action(SetAction(
+        1,
+        ActionType.ORDER,
+        1770816625873,
+        BulkOrder(
+            [
+                OrderRequest(
+                    OrderType.TRIGGER,
+                    42,
+                    True,
+                    "1992",
+                    "0.512",
+                    False,
+                    Trigger(
+                        False,
+                        "90000",
+                        TriggerType.TP,
+                    ),
+                ),
+            ],
+            Grouping.NA,
+            BuilderInfo(
+                bytes.fromhex("c0708cdd6cd166d51da264e3f49a0422be26e35b"),
+                100,
+            ),
+        ),
+    ))
+    client.set_action(SetAction(
+        1,
+        ActionType.UPDATE_LEVERAGE,
+        1770816626930,
+        UpdateLeverage(
+            42,
+            False,
+            8,
+        ),
+    ))
+    client.set_action(SetAction(
+        1,
+        ActionType.APPROVAL_BUILDER_FEE,
+        1770816625903,
+        ApproveBuilderFee(
+            42161,
+            "0.15%",
+            bytes.fromhex("d8dA6BF26964aF9D7eEd9e03E53415D37aA96045"),
+        ),
+    ))
+    client.sign_action("m/44'/60'/0'/0/0")
+    client.sign_action("m/44'/60'/0'/0/1")
+    client.sign_action("m/44'/60'/0'/0/1")
+
+def test_sign_action_trigger_market(backend: BackendInterface) -> None:
+    client = CommandSender(backend)
+    client.provide_action_metadata(ActionMetadata(
+        1,
+        ActionType.ORDER,
+        42,
+        "BTC",
+        Network.MAINNET))
+    client.set_action(SetAction(
+        1,
+        ActionType.ORDER,
+        1770816625873,
+        BulkOrder(
+            [
+                OrderRequest(
+                    OrderType.TRIGGER,
+                    42,
+                    True,
+                    "1992",
+                    "0.512",
+                    False,
+                    Trigger(
+                        True,
+                        "90000",
+                        TriggerType.SL,
+                    ),
+                ),
+            ],
+            Grouping.NA,
+            BuilderInfo(
+                bytes.fromhex("c0708cdd6cd166d51da264e3f49a0422be26e35b"),
+                100,
+            ),
+        ),
+    ))
+    client.set_action(SetAction(
+        1,
+        ActionType.UPDATE_LEVERAGE,
+        1770816626930,
+        UpdateLeverage(
+            42,
+            False,
+            8,
+        ),
+    ))
+    client.set_action(SetAction(
+        1,
+        ActionType.APPROVAL_BUILDER_FEE,
+        1770816625903,
+        ApproveBuilderFee(
+            42161,
+            "0.15%",
+            bytes.fromhex("d8dA6BF26964aF9D7eEd9e03E53415D37aA96045"),
+        ),
+    ))
+    client.sign_action("m/44'/60'/0'/0/0")
+    client.sign_action("m/44'/60'/0'/0/1")
+    client.sign_action("m/44'/60'/0'/0/1")
+
+def test_sign_action_cancel(backend: BackendInterface) -> None:
+    client = CommandSender(backend)
+    client.provide_action_metadata(ActionMetadata(
+        1,
+        ActionType.CANCEL,
+        42,
+        "BTC",
+        Network.MAINNET))
+    client.set_action(SetAction(
+        1,
+        ActionType.CANCEL,
+        1770816625873,
+        BulkCancel([
+            CancelRequest(42, 1),
+        ]),
+    ))
     client.sign_action("m/44'/60'/0'/0/1")
