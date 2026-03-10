71d08e415fc8eca04b6ceed232c96d33f0a1f639
Alexandre Paillier <alexandre.paillier@ledger.fr>
2026-03-10 10:48:02 +0100

    Limit order Ragger test

--- tests/standalone/test_full_flow.py
+++ tests/standalone/test_full_flow.py
@@ -2,7 +2,7 @@ from application_client.action_metadata import ActionMetadata, Network, Operatio
 from application_client.approve_builder_fee import ApproveBuilderFee
 from application_client.bulk_order import BuilderInfo, BulkOrder, Grouping
 from application_client.command_sender import CommandSender
-from application_client.order_request import Limit, OrderRequest, OrderType, Tif
+from application_client.order_request import Limit, OrderRequest, OrderType, Tif, Trigger, TriggerType
 from application_client.set_action import ActionType, SetAction
 from application_client.update_leverage import UpdateLeverage
 from ragger.backend.interface import BackendInterface
@@ -53,6 +53,87 @@ def test_sign_market_order(backend: BackendInterface) -> None:
                 ),
             ],
             Grouping.NA,
+            BuilderInfo(
+                bytes.fromhex("c0708cdd6cd166d51da264e3f49a0422be26e35b"),
+                100,
+            ),
+        ),
+    ))
+    client.sign_action("m/44'/60'/0'/0/0")
+    client.sign_action("m/44'/60'/0'/0/0")
+    client.sign_action("m/44'/60'/0'/0/0")
+
+def test_sign_limit_order(backend: BackendInterface) -> None:
+    client = CommandSender(backend)
+    client.provide_action_metadata(ActionMetadata(
+        1,
+        OperationType.ORDER,
+        1,
+        "ETH",
+        Network.MAINNET))
+    client.set_action(SetAction(
+        1,
+        ActionType.APPROVAL_BUILDER_FEE,
+        1772544778962,
+        ApproveBuilderFee(
+            42161,
+            "0.1000%",
+            bytes.fromhex("c0708cdd6cd166d51da264e3f49a0422be26e35b"),
+        ),
+    ))
+    client.set_action(SetAction(
+        1,
+        ActionType.UPDATE_LEVERAGE,
+        1772544778963,
+        UpdateLeverage(
+            1,
+            False,
+            10,
+        ),
+    ))
+    client.set_action(SetAction(
+        1,
+        ActionType.BULK_ORDER,
+        1772544778964,
+        BulkOrder(
+            [
+                OrderRequest(
+                    OrderType.LIMIT,
+                    1,
+                    False,
+                    "2200",
+                    "0.5128",
+                    False,
+                    Limit(Tif.GTC),
+                ),
+                OrderRequest(
+                    OrderType.TRIGGER,
+                    1,
+                    True,
+                    "500",
+                    "0.5128",
+                    True,
+                    Trigger(
+                        True,
+                        "500",
+                        TriggerType.TP,
+                    ),
+                ),
+                OrderRequest(
+                    OrderType.TRIGGER,
+                    1,
+                    True,
+                    "2500",
+                    "0.5128",
+                    True,
+                    Trigger(
+                        True,
+                        "2500",
+                        TriggerType.SL,
+                    ),
+                ),
+            ],
+            Grouping.NORMAL_TPSL,
             BuilderInfo(
                 bytes.fromhex("c0708cdd6cd166d51da264e3f49a0422be26e35b"),
                 100,
