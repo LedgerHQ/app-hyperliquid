421df10da363a5e8671edf4cd848920c4a117d0d
Alexandre Paillier <alexandre.paillier@ledger.fr>
2026-06-08 13:26:13 +0200

    Lint codebase with clang-format-21

--- src/ui/menu_nbgl.c
+++ src/ui/menu_nbgl.c
@@ -36,8 +36,8 @@ void app_quit(void) {
 //  --------------------- SETTINGS MENU -----------------------
 //  -----------------------------------------------------------
 #define SETTING_INFO_NB 2
-static const char* const INFO_TYPES[SETTING_INFO_NB] = {"Version", "Developer"};
-static const char* const INFO_CONTENTS[SETTING_INFO_NB] = {APPVERSION, "Ledger"};
+static const char *const INFO_TYPES[SETTING_INFO_NB] = {"Version", "Developer"};
+static const char *const INFO_CONTENTS[SETTING_INFO_NB] = {APPVERSION, "Ledger"};
 
 static const nbgl_contentInfoList_t infoList = {
     .nbInfos = SETTING_INFO_NB,
