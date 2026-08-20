a5c4c1cbc9410243357327f2af04f22c0fb15230
Aymeric Robert <aymeric.robert@ledger.fr>
2026-08-20 14:40:25 +0200

    ci: align the ClusterFuzzLite workflow with the other apps

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,28 +1,8 @@
 #!/bin/bash -eu
-
+# ClusterFuzzLite build: delegate to the shared SDK script.
 export BOLOS_SDK=/ledger-secure-sdk
+export APP_DIR=/app
 
 git submodule update --init
 
-# build fuzzers using the docker images.
-pushd fuzzing
-cmake -S . -B build -DCMAKE_C_COMPILER=clang -DCMAKE_BUILD_TYPE=Debug \
-            -G Ninja -DCMAKE_EXPORT_COMPILE_COMMANDS=On \
-            -DBOLOS_SDK="${BOLOS_SDK}" -DTARGET=flex \
-            -DAPP_BUILD_PATH=/app
-
-# Generates .zip for initial corpus in clusterFuzz
-for dir in harness/*; do
-    if [ -d "${dir}" ]; then
-        fuzzer_name=$(basename "${dir}")
-        zip_name="${fuzzer_name}_seed_corpus.zip"
-        echo "Zipping corpus from ${dir} into ${zip_name}"
-
-        (cd "${dir}" && zip -q -r "${zip_name}" .)
-
-        mv "${dir}/${zip_name}" "${OUT}"
-    fi
-done
-cmake --build build
-mv ./build/fuzz_* "${OUT}"
-popd
+exec "${BOLOS_SDK}/fuzzing/scripts/cfl-build.sh"
